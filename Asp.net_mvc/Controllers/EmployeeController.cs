c1cf2e0d7c0df2903e016b9215cafc5b4f86c289
Baldev Bhattarai <bhattarai.baldev@gmail.com>
2018-05-01 13:32:52 -0500

    committed twice

---
+++ Asp.net_mvc/Controllers/EmployeeController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Asp.net_mvc.Models;
+
+namespace Asp.net_mvc.Controllers
+{
+    public class EmployeeController : Controller
+    {
+       
+        public ActionResult Details()
+        {
+
+            
+            EmployeeContext1Entities context = new EmployeeContext1Entities();
+
+
+            //  Employee employee = context.Employees.Single(x => x.employeeId == 1);
+            Employee employee = new Employee()
+            {
+                employeeId = 4,
+                name = "ram",
+                city = "ktm",
+                gender = "male"
+
+            };
+
+            context.Employees.Add(employee);
+            context.SaveChanges();
+
+          // Employee employee1 = context.Employees.Single(x => x.employeeId == 3);
+            List<Employee> employees = context.Employees.ToList();
+            return View(employees);
+        }
+    }
+}
